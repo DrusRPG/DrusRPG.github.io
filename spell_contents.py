fdaa420bc005438a803e01760c563fd0381ccbc2
Drus <eda.rindt@gmail.com>
2025-12-14 22:32:02 +0100

    Ctvrty pokus o tooltipy

--- spell_contents.py
+++ spell_contents.py
@@ -169,6 +169,9 @@ for magic_school_name, magic_school_file, magic_school_image, magic_school_image
 
     # save the spells as hugo markdown. This includes a header for each spell tier and a markdown list for all the individual spells
 #    for spell_tier, spell_list in spells.items():
+#        school_out += '\n'.join([f"\n## {spell_tier.title()}", *["* " + spell for spell in spell_list]])
+
+#    for spell_tier, spell_list in spells.items():
 #        ##school_out += '\n'.join([f"\n## {spell_tier.title()}", *["* " + [spell](a spell_descriptions[spell.strip()]) for spell in spell_list]]) 
 #        school_out += '\n'.join([
 #            f"\n## {spell_tier.title()}",
@@ -179,7 +182,7 @@ for magic_school_name, magic_school_file, magic_school_image, magic_school_image
     for spell_tier, spell_list in spells.items():
         school_out += '\n'.join([
             f"\n## {spell_tier.title()}",
-            *(f"* [{spell}]({spell_descriptions[spell_names[spell.strip()]]})" for spell in spell_list)
+            *(f"* {[{spell}]({spell_descriptions[spell_names[spell.strip()]]})}" for spell in spell_list)
         ])
 
     # save the resulting markdown
